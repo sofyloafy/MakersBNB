1d0b270d7c782e8f81204bb9ae77d47d8faba9bd
Sophie <sophiebrown438@gmail.com>
2020-06-18 17:38:19 +0100

    Attempts to link HTML form to server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const db = require('./database/queries')
+var bodyParser = require('body-parser')
 const app = express()
 const port = 4000
+
+
+app.use(bodyParser.json());
+
+var property = new db.Property()
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -13,8 +20,15 @@ app.get('/sign-in', function (request, response) {
   response.render('loginPage')
 });
 
-app.get('/profile', function (request, response) {
-  response.render('profile')
+app.get('/profile', function (req, res) {
+  property.viewProperties()
+  res.render('profile')
+});
+
+
+app.post('/newproperty', function (req) {
+  const { title, location, description, price, host } = req.body
+  property.insertProperty(title, location, description, price, host)
 });
 
 
